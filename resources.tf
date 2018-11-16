bcaa8d6411cf6ea8c29c9e9c6fcca5d5d4279104
sola <sol@lacad.com>
2018-11-16 22:31:39 +0000

    resources.tf modification

--- resources.tf
+++ resources.tf
@@ -6,3 +6,11 @@ resource "aws_vpc" "environment-example-two" {
     Name = "terraform_aws-vpc-example-two"
   }
 }
+
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-1"
+}
+
