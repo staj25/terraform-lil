4ea2d3ac1a8b8ebe2536470d97c9dfb65f99da66
sola <sol@lacad.com>
2018-11-18 00:30:55 +0000

    subnets & sec grp added

--- resources.tf
+++ resources.tf
@@ -11,6 +11,30 @@ resource "aws_vpc" "environment-example-two" {
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.environment-example-two.id}"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
+}
+
+
+resource "aws_subnet" "subnet2" { 
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-1b"
+}
+
+
+
+resource "aws_security_group" "subnetsecurity" {
+  name = "subnetsecurity"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+   cidr_blocks = [
+     "${aws_vpc.environment-example-two.cidr_block}"
+   ]
+
+   from_port = 80
+   to_port = 80
+   protocol = "tcp"
+   }
 }
 
