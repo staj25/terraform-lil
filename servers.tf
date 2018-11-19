959bbf7aa529699214449fce94a70d4d70127a53
sola <sol@lacad.com>
2018-11-19 17:32:26 +0000

    rectified mistakes in servers.tf

--- servers.tf
+++ servers.tf
@@ -6,14 +6,27 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trust
-}
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
 
+  filter {
+    name = "virtualization-type"
+    values=["hvm"]
+  }
+
+
+  owners = ["099720109477"]
+
+}
 
 
 resource "aws_instance" "secondserver" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "identifiertag"
+  }
+   
 
 }
-
-
-
