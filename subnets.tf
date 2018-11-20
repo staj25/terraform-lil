2697458a5ff6513b4e4982a09e0b6bea3c5f9072
sola <sol@lacad.com>
2018-11-20 08:39:15 +0000

    changes

--- subnets.tf
+++ subnets.tf
@@ -1,40 +1,44 @@
+
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/home/eon01/.aws/credentials"
+  profile                 = "terraform"
+}
+
+
 resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
+
   tags {
     Name = "terraform_aws-vpc-example-two"
   }
 }
 
-
 resource "aws_subnet" "subnet1" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.environment-example-two.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-east-1a"
 }
 
-
-resource "aws_subnet" "subnet2" { 
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
-  vpc_id = "${aws_vpc.environment-example-two.id}"
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-east-1b"
 }
 
-
-
 resource "aws_security_group" "subnetsecurity" {
-  name = "subnetsecurity"
+  name   = "subnetsecurity"
   vpc_id = "${aws_vpc.environment-example-two.id}"
 
   ingress {
-   cidr_blocks = [
-     "${aws_vpc.environment-example-two.cidr_block}"
-   ]
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}",
+    ]
 
-   from_port = 80
-   to_port = 80
-   protocol = "tcp"
-   }
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+  }
 }
-
